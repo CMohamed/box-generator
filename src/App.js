e5d8401aaf57a5bedcba931ea6fe1a48edc98c82
CHADAD <chadad.mohamed@gmail.com>
2022-09-12 20:23:38 +0100

    feat: create box tree and use box in box

--- src/App.js
+++ src/App.js
@@ -1,14 +1,26 @@
 import './App.css';
 import {Box} from "./components/Box";
+import {useState} from "react";
 
 function App() {
-  return (
-    <div>
-        <div id="play-ground" style={{padding: 100}}>
-            <Box />
+    const [boxes, setBoxes] = useState([{
+        id: 0
+    }]);
+    const deleteBoxById = (selectedId) => {
+        const newBoxes = boxes.filter(({id}) => id !== selectedId);
+        setBoxes([...newBoxes]);
+    }
+    return (
+        <div>
+            <div id="play-ground" style={{padding: 100}}>
+                {
+                    boxes.map(box => {
+                        return <Box key={box.id} id={box.id} deleteById={deleteBoxById} />
+                    })
+                }
+            </div>
         </div>
-    </div>
-  );
+    );
 }
 
 export default App;
--- src/components/Box.js
+++ src/components/Box.js
@@ -1,16 +1,35 @@
+import {useState} from "react";
 
 
-export const Box = ({props}) => {
-    const deleteBox = () => {};
-    const addBox = () => {};
-    return <div>
-        <div style={{display: 'flex', justifyContent: 'space-around', padding: '10px', border: '3px solid black'}}>
-            <input/>
-            <button onClick={deleteBox} >delete box</button>
-            <button onClick={addBox} >add box box</button>
+export const Box = (props) => {
+    const [boxes, setBoxes] = useState([]);
+    const deleteBox = () => {
+        console.log('delete this' + props.id);
+        props.deleteById(props.id);
+    };
+    const addBox = () => {
+        const newBox = {id: boxes.length + 1};
+        const newBoxes = [...boxes, newBox];
+        setBoxes([...newBoxes]);
+    };
+    const deleteBoxById = (selectedId) => {
+        console.log('delete in box: ', props.id)
+        console.log('the box: ', selectedId)
+        const newBoxes = boxes.filter(({id}) => id !== selectedId);
+        setBoxes([...newBoxes]);
+    }
+    return <div id={props.id} style={{backgroundColor: `333333${props.id}`}}>
+            <div style={{display: 'flex', justifyContent: 'space-around', padding: '10px', border: '3px solid black'}}>
+                <input/>
+                <button onClick={deleteBox} >delete box</button>
+                <button onClick={addBox} >add box box</button>
+            </div>
+            <div style={{padding: 10}}>
+                {
+                    boxes.map(box => {
+                        return (<Box key={props.id +'-'+ box.id} id={box.id} deleteById={deleteBoxById} />)
+                    })
+                }
+            </div>
         </div>
-        <div>
-            children
-        </div>
-    </div>
 }
